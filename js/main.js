2b21497639aedd542aa6ff95467e811a7f4ec593
Jaco van Stryp <jacovanstryp@gmail.com>
2020-03-22 20:16:32 +0200

    Minior UI Improvements

--- js/main.js
+++ js/main.js
@@ -6,16 +6,14 @@ var id = setInterval(updateExample, 5000); //this is an interval based variable 
 
 //Function updates the example item based of a set of arrays using a random number generator
 function updateExample() {
-    var smartValue = ["Chocolate", "TV", "Tissue Box", "Paper", "Floor", "Glasses", "Newspaper", "Glow Stick", "Soy Sauce Packet", "Lamp Shade", "Bracelet", "Candy Wrapper", "Lip Gloss", "Smart Watch", "Soap", "Tooth picks", "Sun Glasses", "Photo Album", "Video Games", "Bag"]
+    var smartValue = ["Hand Sanitiser", "TV", "Tissue Box", "Paper", "Floor", "Glasses", "Newspaper", "Glow Stick", "Soy Sauce Packet", "Lamp Shade", "Bracelet", "Candy Wrapper", "Lip Gloss", "Smart Watch", "Soap", "Tooth picks", "Sun Glasses", "Photo Album", "Video Games", "Bag"]
     var num = Math.floor(Math.random() * 19) + 1
-    console.log(num); //TODO remove line
     document.getElementById('index_search_input').placeholder = "Eg. " + smartValue[num];
 }
 
 //Function will search through the existing database for data relating the users search query
 function searchDatabase() {
     var searchValue = document.getElementById('index_search_input').value; //gets the VALUE of custom ID
-    console.log(searchValue); //Logs to the console //TODO remove line
     //   document.getElementById('index_search_input').value = ""; //Sets the value
     if (searchValue != "") {
         window.open('http://google.com/search?q=' + searchValue); //opens a new URL

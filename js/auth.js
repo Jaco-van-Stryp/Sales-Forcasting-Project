cedb1b4088205157e6f654cdd43d6306880bb984
Freislich <61614845+Freislich@users.noreply.github.com>
2020-04-28 19:43:33 +0200

    sign up

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+const signin_button_reg = document.querySelector('#signin_button_reg');
+signin_button_reg.addEventListener('btns_r', (e) => {
+     e.preventDefault();
+
+     //get user info
+     const email = signin_button_reg['client_info'].value;
+     const password = signin_button_reg['client_info1'].value;
+     console.log(email,password)
+ })

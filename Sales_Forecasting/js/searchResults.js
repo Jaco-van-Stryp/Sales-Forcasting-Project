670e7058b883138e081b83e7334487ed9a9e9aab
Jaco van Stryp <jacovanstryp@gmail.com>
2020-10-28 18:34:29 +0200

    Update searchResults.js

--- Sales_Forecasting/js/searchResults.js
+++ Sales_Forecasting/js/searchResults.js
@@ -94,6 +94,8 @@ curDocumentData.get().then(function(doc) {
         // doc.data() will be undefined in this case
         console.log("No such document!");
     }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
 });
 loadTable(Predictions)
 
